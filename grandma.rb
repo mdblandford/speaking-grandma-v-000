920ad504241ac0d2d05b6e36ca009efca5243363
Michael Blandford <mdblandford@gmail.com>
2018-06-18 18:30:14 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
-    "HUH?! SPEAK UP, SONNY!" 
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
